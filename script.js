0abaf891372e96bcde8243f5387bd4459547ed9a
MaebhNiGhuairim <84379587+MaebhNiGhuairim@users.noreply.github.com>
2021-06-23 21:08:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const score = document.getElementById('Score')
+const rolls = document.getElementById('Rolls')
+const wins = document.getElementById('Wins')
+
+const diceImage = document.getElementById('diceImage')
+
+const rollDice = document.getElementById('rollDice')
+
+let currentScore = 0 
+let currentRolls = 0
+let currentWins = 0 
+
+const updateStats = () =>{
+    score.textContent = `Score: ${currentScore}`
+    rolls.textContent = `Rolls: ${currentRolls}`
+    wins.textContent = `Wins: ${currentWins}`
+}
+
+rollDice.addEventListener('click', () => {
+    let randomNumber = Math.ceil(Math.random() * 6)
+    diceImage.src = `./diceImages/dice${randomNumber}.png`
+
+    if(randomNumber == 1){
+        currentScore = 0
+    }else{
+        currentScore += randomNumber
+    }
+
+    if(currentScore >= 21){
+        currentWins ++
+        currentScore = 0
+        currentRolls = 0
+    }
+
+
+    currentRolls++
+    updateStats()
+})
